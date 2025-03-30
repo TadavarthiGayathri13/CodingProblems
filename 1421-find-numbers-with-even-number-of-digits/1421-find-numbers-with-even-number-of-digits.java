62b6c241be551fbb5c608ebc4a5b6cf2ee683546
TadavarthiGayathri13 <140795185+TadavarthiGayathri13@users.noreply.github.com>
2025-03-30 22:38:47 +0530

    Time: 1 ms (98.44%), Space: 42.5 MB (97.74%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -17,15 +17,15 @@ class Solution {
         if(num < 0) {
             num = num *-1;
         }
-        // if(num ==0 ) {
-        //     return 1;
-        // }
-        // int count = 0;
-        // while(num  > 0) {
-        //     count ++;
-        //     num = num/10;
-        // }
-        // return count;
-        return (int)(Math.log10(num)) +1;
+        if(num ==0 ) {
+            return 1;
+        }
+        int count = 0;
+        while(num  > 0) {
+            count ++;
+            num = num/10;
+        }
+        return count;
+        // return (int)(Math.log10(num)) +1;    it will find the number of digits 
     }
 }
