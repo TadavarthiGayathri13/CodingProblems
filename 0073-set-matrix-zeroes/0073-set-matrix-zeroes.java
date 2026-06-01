0d436ce97573aa77738c3c609f0ae7a8cbf22a0a
Tadavarthi Gayathri <140795185+TadavarthiGayathri13@users.noreply.github.com>
2026-06-01 22:52:53 +0530

    Time: 1 ms (98.29%), Space: 47.5 MB (79.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,14 +1,10 @@
-
-
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-
         boolean[] rows = new boolean[m];
         boolean[] cols = new boolean[n];
 
-        // Find original zeros
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -17,8 +13,6 @@ class Solution {
                 }
             }
         }
-
-        // Set rows and columns to zero
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (rows[i] || cols[j]) {
